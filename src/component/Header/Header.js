807b99ef4fde20bb105c963e65c0dd0a6d178665
asifalam515 <asibulalam515@gmail.com>
2022-04-08 19:14:17 +0600

    header added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
   return (
     <div className='bg-white'>
  <div>   
-
+<img src="https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_300,h_100/https://www.bpa-solutions.net/wp-content/uploads/2022/01/BPA-logo-300x67.png" alt="" />
     <h1 className='bg-warning top'> BUSINESS PROFESSIONALS ACADEMY </h1>
   <div className='text-img'>
   <div className='header-text'>
